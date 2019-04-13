a95762a70ee2f84af7cab7689a43107d7ecba1d0
Paul Villalobos <wilverpaul@gmail.com>
2019-04-12 23:21:45 -0500

    Tarea Dynamic Components

---
+++ Tarea 8 - Slots & Dynamic Components/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="componenteSeleccionado='app-blue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="componenteSeleccionado='app-green'">Load Green Template</button>
+                <button class="btn btn-danger" @click="componenteSeleccionado='app-red'">Load Red Template</button>
+                <hr>
+
+                <component :is="componenteSeleccionado">
+                    <span slot="nombre">Elvia Gavidia</span>
+                    <span slot="cargo">Mi Esposita</span>
+
+                    <span slot="comentario">Te amo puerquita :)</span>
+                </component>
+
+                <!-- <app-blue>
+                    <span slot="nombre">Elvia Gavidia</span>
+                    <span slot="cargo">Mi Esposita</span>
+
+                    <span slot="comentario">Te amo puerquita :)</span>
+                </app-blue>
+                <app-green>
+                    <span slot="nombre">Wilver Villalobos</span>
+                    <span slot="cargo">Papito</span>
+
+                    <span slot="comentario">Viejo</span>
+                </app-green>
+                <app-red>
+                    <span slot="nombre">Eliana Castañeda</span>
+                    <span slot="cargo">Mamota</span>
+
+                    <span slot="comentario">Renegona</span>
+                </app-red> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data: function() {
+            return {
+                componenteSeleccionado: 'app-blue'
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Tarea 8 - Slots & Dynamic Components/src/components/Green.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>Nombre:
+        <slot name="nombre"></slot></p>
+
+        <p>Cargo: <slot name="cargo"></slot></p>
+<hr>
+        <slot name="comentario"></slot>
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid green;
+        background-color: lightgreen;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
